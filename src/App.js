727709c3ac3c8b2c82fe4317e46b260c94a595b2
Andy Goh <andyh.goh@gmail.com>
2022-10-14 13:22:02 -0700

    initial setup for calendar block

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 import reactLogo from './logo-react.svg';
 import djangoLogo from './logo-django.svg';
+import CalendarBlock from './components/CalendarBlock';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={reactLogo} className="App-logo" alt="React logo" />
-          <img src={djangoLogo} className="App-logo" alt="Django logo" />
-          <h1 className="App-title">Welcome to the React-Django app</h1>
-        </header>
-        <p className="App-intro">
-          I'm a blank canvas waiting for a coder's touch!
-        </p>
+        <CalendarBlock />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CalendarBlock from './components/CalendarBlock';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<CalendarBlock />, document.getElementById('root'));
 registerServiceWorker();
