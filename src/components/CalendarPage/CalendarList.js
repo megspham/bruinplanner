9578dda4b523a30786e2fb9e9adcf53f58eeb4fb
Andy Goh <andyh.goh@gmail.com>
2022-10-17 20:23:07 -0700

    change calendar to be responsive and add summer quarter

--- src/components/CalendarPage/CalendarList.js
+++ src/components/CalendarPage/CalendarList.js
@@ -7,49 +7,61 @@ class CalendarList extends Component {
     return (
       <div>
         <div className="CalendarRow">
-          <CalendarBlock color = "#005587" 
-                calendarDate={"Fall " + new Date(new Date().setFullYear(this.currYear)).getFullYear()
-}>
-</CalendarBlock>
-          <CalendarBlock color = "#005587" 
+          <CalendarBlock color="#005587"
+            calendarDate={"Fall " + new Date(new Date().setFullYear(this.currYear)).getFullYear()
+            }>
+          </CalendarBlock>
+          <CalendarBlock color="#005587"
             calendarDate={"Winter " + new Date(new Date().setFullYear(this.currYear + 1)).getFullYear()}
           ></CalendarBlock>
-          <CalendarBlock color = "#005587" 
+          <CalendarBlock color="#005587"
             calendarDate={"Spring " + new Date(new Date().setFullYear(this.currYear + 1)).getFullYear()}
           ></CalendarBlock>
+          <CalendarBlock color="#005587"
+            calendarDate={"Summer " + new Date(new Date().setFullYear(this.currYear + 1)).getFullYear()}
+          ></CalendarBlock>
         </div>
 
         <div className="CalendarRow">
-          <CalendarBlock color = "#8BB8E8" 
-          calendarDate={"Fall " + new Date(new Date().setFullYear(this.currYear + 1)).getFullYear()}
+          <CalendarBlock color="#8BB8E8"
+            calendarDate={"Fall " + new Date(new Date().setFullYear(this.currYear + 1)).getFullYear()}
           ></CalendarBlock>
-          <CalendarBlock color = "#8BB8E8"
+          <CalendarBlock color="#8BB8E8"
             calendarDate={"Winter " + new Date(new Date().setFullYear(this.currYear + 2)).getFullYear()}
           ></CalendarBlock>
-          <CalendarBlock color = "#8BB8E8"
+          <CalendarBlock color="#8BB8E8"
             calendarDate={"Spring " + new Date(new Date().setFullYear(this.currYear + 2)).getFullYear()}
           ></CalendarBlock>
+          <CalendarBlock color="#8BB8E8"
+            calendarDate={"Summer " + new Date(new Date().setFullYear(this.currYear + 2)).getFullYear()}
+          ></CalendarBlock>
         </div>
 
         <div className="CalendarRow">
-          <CalendarBlock color = "#FFE9AD"
-          calendarDate={"Fall " + new Date(new Date().setFullYear(this.currYear + 2)).getFullYear()}></CalendarBlock>
-          <CalendarBlock color = "#FFE9AD"
+          <CalendarBlock color="#FFE9AD"
+            calendarDate={"Fall " + new Date(new Date().setFullYear(this.currYear + 2)).getFullYear()}></CalendarBlock>
+          <CalendarBlock color="#FFE9AD"
             calendarDate={"Winter " + new Date(new Date().setFullYear(this.currYear + 3)).getFullYear()}
           ></CalendarBlock>
-          <CalendarBlock color = "#FFE9AD"
+          <CalendarBlock color="#FFE9AD"
             calendarDate={"Spring " + new Date(new Date().setFullYear(this.currYear + 3)).getFullYear()}
+          ></CalendarBlock>
+          <CalendarBlock color="#FFE9AD"
+            calendarDate={"Summer " + new Date(new Date().setFullYear(this.currYear + 3)).getFullYear()}
           ></CalendarBlock>
         </div>
 
         <div className="CalendarRow">
-          <CalendarBlock color = "#005587"
-          calendarDate={"Fall " + new Date(new Date().setFullYear(this.currYear + 3)).getFullYear()}></CalendarBlock>
-          <CalendarBlock color = "#005587"
+          <CalendarBlock color="#005587"
+            calendarDate={"Fall " + new Date(new Date().setFullYear(this.currYear + 3)).getFullYear()}></CalendarBlock>
+          <CalendarBlock color="#005587"
             calendarDate={"Winter " + new Date(new Date().setFullYear(this.currYear + 4)).getFullYear()}
           ></CalendarBlock>
-          <CalendarBlock color = "#005587"
+          <CalendarBlock color="#005587"
             calendarDate={"Spring " + new Date(new Date().setFullYear(this.currYear + 4)).getFullYear()}
+          ></CalendarBlock>
+          <CalendarBlock color="#005587"
+            calendarDate={"Summer " + new Date(new Date().setFullYear(this.currYear + 4)).getFullYear()}
           ></CalendarBlock>
         </div>
       </div>
