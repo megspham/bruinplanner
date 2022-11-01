515c97062c16864f7dc47b15f75becff54a0df4a
blerdwor <bellelee@g.ucla.edu>
2022-10-31 22:38:00 -0700

    Images up

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BackgroundSvg from "/home/belle/bruinplanner/src/components/images/LandingPageBackground.svg";
+
+const Background = () => {
+  return (
+    <div className="App">
+      <img src={BackgroundSvg} alt="Your SVG" />
+    </div>
+  );
+};
+
+export default Background;
