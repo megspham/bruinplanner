404d7cedae761683608f15e51ff71df521840601
blerdwor <bellelee@g.ucla.edu>
2022-10-31 22:17:51 -0700

    Revert "Image up on local (but path is specific to Belle)"
    This reverts commit df3283e47aa9ceec8f5403a139598558faa8cc3f.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import CalendarList from './components/CalendarPage/CalendarList.js';
-import Background from './components/LandingPage/LandingPage.js';
 
 class App extends Component {
   render() {
     return (
       <div className="BruinPlanner">
-        <Background />        
+        <CalendarList />        
       </div>
     );
   }
