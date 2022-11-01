df3283e47aa9ceec8f5403a139598558faa8cc3f
blerdwor <bellelee@g.ucla.edu>
2022-10-31 22:11:45 -0700

    Image up on local (but path is specific to Belle)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import CalendarList from './components/CalendarPage/CalendarList.js';
+import Background from './components/LandingPage/LandingPage.js';
 
 class App extends Component {
   render() {
     return (
       <div className="BruinPlanner">
-        <CalendarList />        
+        <Background />        
       </div>
     );
   }
