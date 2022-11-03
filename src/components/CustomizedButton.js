826537db38d14a37788bfced45d11ae03da423b1
Megan Pham <meganpham@Megans-MacBook-Pro-3.local>
2022-11-02 19:45:32 -0700

    customized button and drag/drop

---
+++ src/components/CustomizedButton.js
@@ -0,0 +1,27 @@
+import React from "react"; 
+import "./CustomizedButton.css"; 
+
+
+export function CustomizedButton(text, funct) {
+    const style = {
+        width: '524px',
+        height: '88px',
+        background: '#FFFFFF',
+        color: '#757575',
+        boxShadow: '0px 0px 13.1034px #8BB8E8',
+        border: 'none',
+        fontFamily: 'Montserrat',
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: '30.6431px',
+        lineHeight: '37px'
+    }
+    return (
+        <div className="myButton">
+            <button type="button" style={style} onClick={funct}>
+                {text}
+            </button>
+        </div>
+    )
+}
+
