33e0652c96e3063d705f6bfe46ef721f40af34ba
Megan Pham <meganpham@Megans-MacBook-Pro-3.local>
2022-11-02 23:46:06 -0700

    react-router

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
 import "./LandingPage.css";
-import BackgroundSvg from "/home/belle/bruinplanner/src/components/images/LandingPageBackground.svg";
+import BackgroundSvg from "../images/LandingPageBackground.svg";
+import { CustomizedButton } from "../CustomizedButton";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
-    <div className="App">
+    <div className="landing">
       <img src={BackgroundSvg} alt="Your SVG" class="center"/>
+      <Link to="/calendar">
+      <div class="button-container">
+        <CustomizedButton class="button" text="ENTER HERE"></CustomizedButton>
+      </div>
+      </Link>
     </div>
   );
 };
