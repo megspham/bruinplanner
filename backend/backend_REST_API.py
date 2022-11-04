d9b171cffb7599a9fb0d56eaa331261599641dd2
Taykhoom Dalal <tdalal@g.ucla.edu>
2022-11-04 10:57:00 -0700

    preliminary addUser api

--- backend/backend_REST_API.py
+++ backend/backend_REST_API.py
@@ -1,8 +1,11 @@
+import db_utils as db
+
 # POST /api/addUser
 def addUser(name, email):
     """
     Make new table for user
     """
+    db.execute("INSERT IGNORE INTO users (name, email, calendar) VALUES (%s, %s, %s);", (name, email, ""))
     return
 
 # POST /api/importDARS
