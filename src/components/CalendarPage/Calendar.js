cd2d8de59d8fa3ddc2030a1bb66f93cfc9a32724
iangalvez <iangalvez@g.ucla.edu>
2022-11-11 10:57:30 -0800

    added Sidebar into Calendar structure

---
+++ src/components/CalendarPage/Calendar.js
@@ -0,0 +1,28 @@
+import React, { Component, useState} from 'react';
+import './Calendar.css';
+import Sidebar from "./Sidebar";
+import CalendarList from './CalendarList';
+import {DndContext} from '@dnd-kit/core';
+
+export default function Calendar() {
+  const [isDropped, setIsDropped] = useState(null);
+  const [classMappings, setClassMappings] = useState([]);
+  return (
+      <div className="BruinPlanner">
+        <DndContext onDragEnd={handleDragEnd} onDragOver={handleDragOver}>
+          <CalendarList/>        
+          <Sidebar />
+        </DndContext>
+      </div>
+  );
+
+  function handleDragEnd({over}) {
+    setIsDropped(over ? over.id : null);
+    setClassMappings()
+    console.log(over);
+  }
+
+  function handleDragOver({over}) {
+    console.log(over);
+  }
+}
