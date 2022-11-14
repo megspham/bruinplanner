c3a0166491c7694364db7673c560b5aa36c6b212
Megan Pham <meganpham@Megans-MacBook-Pro-3.local>
2022-11-14 13:32:30 -0800

    parsed one q and first class

--- src/components/CalendarPage/CalendarBlock.js
+++ src/components/CalendarPage/CalendarBlock.js
@@ -14,7 +14,7 @@ import {Droppable} from '../Droppable';
  * @param {string} calendarDate The name of the quarter (e.g., Fall 2022) 
  * @returns CalendarBlock HTML div object
  */
-function CalendarBlock({ color, calendarDate }) {
+function CalendarBlock({ color, calendarDate, jsonInput }) {
   const [isDropped, setIsDropped] = useState(null); 
   const draggable = (
     <Draggable id ="draggable"> 
@@ -22,12 +22,61 @@ function CalendarBlock({ color, calendarDate }) {
     </Draggable>
   );
 
+  let example = {
+    "calendar": {
+      "quarters": [{
+          "quarter": {
+            "year": 2020,
+            "quarter_name": "WI",
+            "courses": [{
+              "course": {
+                "course_name": "CS 32",
+                "course_department": "CS",
+                "course_description": "Data Structures and Algorithms",
+                "course_units": 4.0,
+                "course_requirement": "CS",
+                "course_pre_requisites": [
+                                  {
+                      "pre_requisite_name": "CS 31"
+                    }
+                              ]
+              }
+            }]
+          }
+        },
+        {
+          "quarter2": {
+            "year": 2020,
+            "quarter_name": "SP",
+            "courses": [{
+              "course": {
+                "course_name": "CS 33",
+                "course_department": "CS",
+                "course_description": "Intro to Computer Architecture",
+                "course_units": 4.0,
+                "course_requirement": "CS",
+                "course_pre_requisites": [
+                                  {
+                      "pre_requisite_name": "CS 31"
+                    }
+                              ]
+              }
+            }]
+          }
+        }
+      ]
+    }
+  };
+
+  let stringInput = JSON.stringify(example);
+  let parsedInput = JSON.parse(stringInput);   
+  
   return (
     <div className="BackgroundBlock" style={{backgroundColor: color}}>
       <div className="CalendarTitle">{calendarDate}</div>
       <div className="DisplayBlock">
         <Droppable id={calendarDate+" Slot 1"}>
-          {isDropped === calendarDate+" Slot 1" ? draggable : 'DROP HERE'}
+          {isDropped === calendarDate+" Slot 1" ? draggable : parsedInput.calendar.quarters[0].quarter.courses[0].course.course_name}
         </Droppable>
         <Droppable id={calendarDate+" Slot 2"}>
           {isDropped === calendarDate+" Slot 2" ? draggable : 'DROP HERE'}
@@ -38,6 +87,7 @@ function CalendarBlock({ color, calendarDate }) {
         <Droppable id={calendarDate+" Slot 4"}>
           {isDropped === calendarDate+" Slot 4" ? draggable : 'DROP HERE'}
         </Droppable>
+        
        </div>
      </div>
   );
