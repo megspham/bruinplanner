7534f21f58f0da3d69cf5a089ad334297b427f44
Andy Goh <andyh.goh@gmail.com>
2022-11-14 15:52:43 -0800

    parse json to populate calendar automatically

--- src/components/CalendarPage/CalendarBlock.js
+++ src/components/CalendarPage/CalendarBlock.js
@@ -14,78 +14,29 @@ import {Droppable} from '../Droppable';
  * @param {string} calendarDate The name of the quarter (e.g., Fall 2022) 
  * @returns CalendarBlock HTML div object
  */
-function CalendarBlock({ color, calendarDate, jsonInput }) {
+function CalendarBlock({ color, calendarDate, courses }) {
   const [isDropped, setIsDropped] = useState(null); 
   const draggable = (
     <Draggable id ="draggable"> 
     DRAG ME
     </Draggable>
   );
-
-  let example = {
-    "calendar": {
-      "quarters": [{
-          "quarter": {
-            "year": 2020,
-            "quarter_name": "WI",
-            "courses": [{
-              "course": {
-                "course_name": "CS 32",
-                "course_department": "CS",
-                "course_description": "Data Structures and Algorithms",
-                "course_units": 4.0,
-                "course_requirement": "CS",
-                "course_pre_requisites": [
-                                  {
-                      "pre_requisite_name": "CS 31"
-                    }
-                              ]
-              }
-            }]
-          }
-        },
-        {
-          "quarter2": {
-            "year": 2020,
-            "quarter_name": "SP",
-            "courses": [{
-              "course": {
-                "course_name": "CS 33",
-                "course_department": "CS",
-                "course_description": "Intro to Computer Architecture",
-                "course_units": 4.0,
-                "course_requirement": "CS",
-                "course_pre_requisites": [
-                                  {
-                      "pre_requisite_name": "CS 31"
-                    }
-                              ]
-              }
-            }]
-          }
-        }
-      ]
-    }
-  };
-
-  let stringInput = JSON.stringify(example);
-  let parsedInput = JSON.parse(stringInput);   
-  
+   
   return (
     <div className="BackgroundBlock" style={{backgroundColor: color}}>
       <div className="CalendarTitle">{calendarDate}</div>
       <div className="DisplayBlock">
         <Droppable id={calendarDate+" Slot 1"}>
-          {isDropped === calendarDate+" Slot 1" ? draggable : parsedInput.calendar.quarters[0].quarter.courses[0].course.course_name}
+          {isDropped === calendarDate+" Slot 1" ? draggable : courses[0]}
         </Droppable>
         <Droppable id={calendarDate+" Slot 2"}>
-          {isDropped === calendarDate+" Slot 2" ? draggable : 'DROP HERE'}
+          {isDropped === calendarDate+" Slot 2" ? draggable : courses[1]}
         </Droppable>
         <Droppable id={calendarDate+" Slot 3"}>
-          {isDropped === calendarDate+" Slot 3" ? draggable : 'DROP HERE'}
+          {isDropped === calendarDate+" Slot 3" ? draggable : courses[2]}
         </Droppable>
         <Droppable id={calendarDate+" Slot 4"}>
-          {isDropped === calendarDate+" Slot 4" ? draggable : 'DROP HERE'}
+          {isDropped === calendarDate+" Slot 4" ? draggable : courses[3]}
         </Droppable>
         
        </div>
--- src/components/CalendarPage/CalendarList.js
+++ src/components/CalendarPage/CalendarList.js
@@ -1,73 +1,157 @@
 /**
  * @file Aggregates multiple CalendarBlocks to form the full calendar.
- * @author Megan Pham
+ * @author Megan Pham, Andy Goh
  */
 import React, { Component } from "react";
 import CalendarBlock from "./CalendarBlock";
 
 class CalendarList extends Component {
   currYear = new Date().getFullYear();
+
+  example = {
+    "calendar": {
+      "quarters": [
+        {
+          "quarter": {
+            "year": 2020,
+            "quarter_name": "WI",
+            "courses": [{
+              "course": {
+                "course_name": "CS 32",
+                "course_department": "CS",
+                "course_description": "Data Structures and Algorithms",
+                "course_units": 4.0,
+                "course_requirement": "CS",
+                "course_pre_requisites": [
+                                  {
+                      "pre_requisite_name": "CS 31"
+                    }
+                              ]
+              }
+            }]
+          }
+        },
+        {
+          "quarter": {
+            "year": 2020,
+            "quarter_name": "SP",
+            "courses": [{
+              "course": {
+                "course_name": "CS 33",
+                "course_department": "CS",
+                "course_description": "Intro to Computer Architecture",
+                "course_units": 4.0,
+                "course_requirement": "CS",
+                "course_pre_requisites": [
+                                  {
+                      "pre_requisite_name": "CS 31"
+                    }
+                              ]
+              }
+            }]
+          }
+        }
+      ]
+    }
+  };
+
+  parse_json(json) {
+    let stringInput = JSON.stringify(json);
+    let parsedInput = JSON.parse(stringInput);
+
+    let start_year = parsedInput.calendar.quarters[0].quarter.year;
+    if (parsedInput.calendar.quarters[0].quarter.name != "FA") {
+      start_year = start_year - 1;
+    }
+    let default_calendar = [];
+    let default_courses =["DROP HERE", "DROP HERE", "DROP HERE", "DROP HERE"];
+    for (let row_idx = 0; row_idx < 4; row_idx ++) {
+      let fall = {
+        'name' : "Fall",
+        'year' : start_year + row_idx,
+        'courses' : default_courses
+      }
+      let winter = {
+        'name' : "Winter",
+        'year' : start_year + 1 + row_idx,
+        'courses' : default_courses
+      }
+      let spring = {
+        'name' : "Spring",
+        'year' : start_year + 1 + row_idx,
+        'courses' : default_courses
+      }
+      let summer = {
+        'name' : "Summer",
+        'year' : start_year + 1 + row_idx,
+        'courses' : default_courses
+      }
+
+      let default_year = [fall, winter, spring, summer];
+      default_calendar.push(default_year);
+    }
+
+    const quarter_name_dict = {
+      "FA" : "Fall",
+      "WI" : "Winter",
+      "SP" : "Spring",
+      "SM" : "Summer"
+    };
+    const quarter_id_dict = {
+      "FA" : 0,
+      "WI" : 1,
+      "SP" : 2,
+      "SM" : 3
+    }
+
+    let json_quarters = parsedInput.calendar.quarters;
+    for (let i = 0; i < json_quarters.length; i++) {
+      const q = json_quarters[i];
+      const year = q.quarter.year;
+      const quarter_name = quarter_name_dict[q.quarter.quarter_name];
+      const quarter_id = quarter_id_dict[q.quarter.quarter_name];
+      let courses = ["DROP HERE", "DROP HERE", "DROP HERE", "DROP HERE"];
+      for (let j = 0; j < q.quarter.courses.length; j++) {
+        courses[j] = q.quarter.courses[j].course.course_name;
+      }
+      const quarter = {
+        'name' : quarter_name,
+        'year' : year,
+        'courses' : courses
+      }
+      default_calendar[Math.floor(i / 4)][quarter_id] = quarter;
+    }
+    return default_calendar;
+  }
+
+  parsed = this.parse_json(this.example);
+
+  get_row(row_idx) {
+    const row = this.parsed[row_idx];
+    const color_dict = {
+      0: "#005587",
+      1: "#8BB8E8",
+      2: "#FFE9AD",
+      3: "#005587"
+    };
+
+    let rows = []
+    for (const quarter of row) {
+      rows.push(<CalendarBlock color={color_dict[row_idx]}
+        calendarDate = {quarter.name + " " + quarter.year}
+        courses = {quarter.courses}>
+      </CalendarBlock>)
+    }
+    return <div className="CalendarRow">{rows}</div>;
+  }
+
   render() {
     return (
       <div>
-        <div className="CalendarRow">
-          <CalendarBlock color="#005587"
-            calendarDate={"Fall " + new Date(new Date().setFullYear(this.currYear)).getFullYear()
-            }>
-          </CalendarBlock>
-          <CalendarBlock color="#005587"
-            calendarDate={"Winter " + new Date(new Date().setFullYear(this.currYear + 1)).getFullYear()}
-          ></CalendarBlock>
-          <CalendarBlock color="#005587"
-            calendarDate={"Spring " + new Date(new Date().setFullYear(this.currYear + 1)).getFullYear()}
-          ></CalendarBlock>
-          <CalendarBlock color="#005587"
-            calendarDate={"Summer " + new Date(new Date().setFullYear(this.currYear + 1)).getFullYear()}
-          ></CalendarBlock>
-        </div>
-
-        <div className="CalendarRow">
-          <CalendarBlock color="#8BB8E8"
-            calendarDate={"Fall " + new Date(new Date().setFullYear(this.currYear + 1)).getFullYear()}
-          ></CalendarBlock>
-          <CalendarBlock color="#8BB8E8"
-            calendarDate={"Winter " + new Date(new Date().setFullYear(this.currYear + 2)).getFullYear()}
-          ></CalendarBlock>
-          <CalendarBlock color="#8BB8E8"
-            calendarDate={"Spring " + new Date(new Date().setFullYear(this.currYear + 2)).getFullYear()}
-          ></CalendarBlock>
-          <CalendarBlock color="#8BB8E8"
-            calendarDate={"Summer " + new Date(new Date().setFullYear(this.currYear + 2)).getFullYear()}
-          ></CalendarBlock>
-        </div>
-
-        <div className="CalendarRow">
-          <CalendarBlock color="#FFE9AD"
-            calendarDate={"Fall " + new Date(new Date().setFullYear(this.currYear + 2)).getFullYear()}></CalendarBlock>
-          <CalendarBlock color="#FFE9AD"
-            calendarDate={"Winter " + new Date(new Date().setFullYear(this.currYear + 3)).getFullYear()}
-          ></CalendarBlock>
-          <CalendarBlock color="#FFE9AD"
-            calendarDate={"Spring " + new Date(new Date().setFullYear(this.currYear + 3)).getFullYear()}
-          ></CalendarBlock>
-          <CalendarBlock color="#FFE9AD"
-            calendarDate={"Summer " + new Date(new Date().setFullYear(this.currYear + 3)).getFullYear()}
-          ></CalendarBlock>
-        </div>
-
-        <div className="CalendarRow">
-          <CalendarBlock color="#005587"
-            calendarDate={"Fall " + new Date(new Date().setFullYear(this.currYear + 3)).getFullYear()}></CalendarBlock>
-          <CalendarBlock color="#005587"
-            calendarDate={"Winter " + new Date(new Date().setFullYear(this.currYear + 4)).getFullYear()}
-          ></CalendarBlock>
-          <CalendarBlock color="#005587"
-            calendarDate={"Spring " + new Date(new Date().setFullYear(this.currYear + 4)).getFullYear()}
-          ></CalendarBlock>
-          <CalendarBlock color="#005587"
-            calendarDate={"Summer " + new Date(new Date().setFullYear(this.currYear + 4)).getFullYear()}
-          ></CalendarBlock>
-        </div>       
+        {this.get_row(0)}
+        {this.get_row(1)}
+        {this.get_row(2)}
+        {this.get_row(3)}
       </div>
     );
   }
