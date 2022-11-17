20858e5d726ca2353313b4359c9410254023fe85
iangalvez <iangalvez@g.ucla.edu>
2022-11-16 16:29:06 -0800

    now can drag and drop classes onto calendar blocks

--- src/components/CalendarPage/CalendarBlock.js
+++ src/components/CalendarPage/CalendarBlock.js
@@ -8,17 +8,17 @@ import "./CalendarBlock.css";
 import {DndContext} from '@dnd-kit/core';
 import Draggable from '../Draggable';
 import {Droppable} from '../Droppable';
-
+import {SidebarButton} from "../SidebarButton"
 /**
  * @param {string} color Defines the color of the block
  * @param {string} calendarDate The name of the quarter (e.g., Fall 2022) 
  * @returns CalendarBlock HTML div object
  */
-function CalendarBlock({ color, calendarDate }) {
+function CalendarBlock({ color, calendarDate, blockId, blockState }) {
   const [isDropped, setIsDropped] = useState(null); 
-  const draggable = (
-    <Draggable id ="draggable"> 
-    DRAG ME
+  const draggable = (index) => (
+    <Draggable key={index} id={index}>
+      <SidebarButton text={"CS "+(index)} />                    
     </Draggable>
   );
 
@@ -26,17 +26,17 @@ function CalendarBlock({ color, calendarDate }) {
     <div className="BackgroundBlock" style={{backgroundColor: color}}>
       <div className="CalendarTitle">{calendarDate}</div>
       <div className="DisplayBlock">
-        <Droppable id={calendarDate+" Slot 1"}>
-          {isDropped === calendarDate+" Slot 1" ? draggable : 'DROP HERE'}
+        <Droppable id={blockId+" 1"}>
+          {blockState[0] ? draggable(blockState[0]) : 'DROP HERE'}
         </Droppable>
-        <Droppable id={calendarDate+" Slot 2"}>
-          {isDropped === calendarDate+" Slot 2" ? draggable : 'DROP HERE'}
+        <Droppable id={blockId+" 2"}>
+          {blockState[1] ? draggable(blockState[1]) : 'DROP HERE'}
         </Droppable>
-        <Droppable id={calendarDate+" Slot 3"}>
-          {isDropped === calendarDate+" Slot 3" ? draggable : 'DROP HERE'}
+        <Droppable id={blockId+" 3"}>
+          {blockState[2] ? draggable(blockState[2]) : 'DROP HERE'}
         </Droppable>
-        <Droppable id={calendarDate+" Slot 4"}>
-          {isDropped === calendarDate+" Slot 4" ? draggable : 'DROP HERE'}
+        <Droppable id={blockId+" 4"}>
+          {blockState[3] ? draggable(blockState[3]) : 'DROP HERE'}
         </Droppable>
        </div>
      </div>
--- src/components/CalendarPage/CalendarList.js
+++ src/components/CalendarPage/CalendarList.js
@@ -7,64 +7,65 @@ import CalendarBlock from "./CalendarBlock";
 
 class CalendarList extends Component {
   currYear = new Date().getFullYear();
+  calendarState = this.props.classMappings;
   render() {
     return (
-      <div>
+      <div className="CalendarList">
         <div className="CalendarRow">
-          <CalendarBlock color="#005587"
+          <CalendarBlock color="#005587" blockId="fa22" blockState={this.calendarState["fa22"]}
             calendarDate={"Fall " + new Date(new Date().setFullYear(this.currYear)).getFullYear()
             }>
           </CalendarBlock>
-          <CalendarBlock color="#005587"
+          <CalendarBlock color="#005587" blockId="wi23" blockState={this.calendarState["wi23"]}
             calendarDate={"Winter " + new Date(new Date().setFullYear(this.currYear + 1)).getFullYear()}
           ></CalendarBlock>
-          <CalendarBlock color="#005587"
+          <CalendarBlock color="#005587" blockId="sp23" blockState={this.calendarState["sp23"]}
             calendarDate={"Spring " + new Date(new Date().setFullYear(this.currYear + 1)).getFullYear()}
           ></CalendarBlock>
-          <CalendarBlock color="#005587"
+          <CalendarBlock color="#005587" blockId="su23" blockState={this.calendarState["su23"]}
             calendarDate={"Summer " + new Date(new Date().setFullYear(this.currYear + 1)).getFullYear()}
           ></CalendarBlock>
         </div>
 
         <div className="CalendarRow">
-          <CalendarBlock color="#8BB8E8"
+          <CalendarBlock color="#8BB8E8" blockId="fa23" blockState={this.calendarState["fa23"]}
             calendarDate={"Fall " + new Date(new Date().setFullYear(this.currYear + 1)).getFullYear()}
           ></CalendarBlock>
-          <CalendarBlock color="#8BB8E8"
+          <CalendarBlock color="#8BB8E8" blockId="wi24" blockState={this.calendarState["wi24"]}
             calendarDate={"Winter " + new Date(new Date().setFullYear(this.currYear + 2)).getFullYear()}
           ></CalendarBlock>
-          <CalendarBlock color="#8BB8E8"
+          <CalendarBlock color="#8BB8E8" blockId="sp24" blockState={this.calendarState["sp24"]}
             calendarDate={"Spring " + new Date(new Date().setFullYear(this.currYear + 2)).getFullYear()}
           ></CalendarBlock>
-          <CalendarBlock color="#8BB8E8"
+          <CalendarBlock color="#8BB8E8" blockId="su24" blockState={this.calendarState["su24"]}
             calendarDate={"Summer " + new Date(new Date().setFullYear(this.currYear + 2)).getFullYear()}
           ></CalendarBlock>
         </div>
 
         <div className="CalendarRow">
-          <CalendarBlock color="#FFE9AD"
+          <CalendarBlock color="#FFE9AD" blockId="fa24" blockState={this.calendarState["fa24"]}
             calendarDate={"Fall " + new Date(new Date().setFullYear(this.currYear + 2)).getFullYear()}></CalendarBlock>
-          <CalendarBlock color="#FFE9AD"
+          <CalendarBlock color="#FFE9AD" blockId="wi25" blockState={this.calendarState["wi25"]}
             calendarDate={"Winter " + new Date(new Date().setFullYear(this.currYear + 3)).getFullYear()}
           ></CalendarBlock>
-          <CalendarBlock color="#FFE9AD"
+          <CalendarBlock color="#FFE9AD" blockId="sp25" blockState={this.calendarState["sp25"]}
             calendarDate={"Spring " + new Date(new Date().setFullYear(this.currYear + 3)).getFullYear()}
           ></CalendarBlock>
-          <CalendarBlock color="#FFE9AD"
+          <CalendarBlock color="#FFE9AD" blockId="su25" blockState={this.calendarState["su25"]}
             calendarDate={"Summer " + new Date(new Date().setFullYear(this.currYear + 3)).getFullYear()}
           ></CalendarBlock>
         </div>
 
         <div className="CalendarRow">
-          <CalendarBlock color="#005587"
+          <CalendarBlock color="#005587" blockId="fa25" blockState={this.calendarState["fa25"]}
             calendarDate={"Fall " + new Date(new Date().setFullYear(this.currYear + 3)).getFullYear()}></CalendarBlock>
-          <CalendarBlock color="#005587"
+          <CalendarBlock color="#005587" blockId="wi26" blockState={this.calendarState["wi26"]}
             calendarDate={"Winter " + new Date(new Date().setFullYear(this.currYear + 4)).getFullYear()}
           ></CalendarBlock>
-          <CalendarBlock color="#005587"
+          <CalendarBlock color="#005587" blockId="sp26" blockState={this.calendarState["sp26"]}
             calendarDate={"Spring " + new Date(new Date().setFullYear(this.currYear + 4)).getFullYear()}
           ></CalendarBlock>
-          <CalendarBlock color="#005587"
+          <CalendarBlock color="#005587" blockId="su26" blockState={this.calendarState["su26"]}
             calendarDate={"Summer " + new Date(new Date().setFullYear(this.currYear + 4)).getFullYear()}
           ></CalendarBlock>
         </div>       
