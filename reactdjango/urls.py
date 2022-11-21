d2590f44c0c90392a5da93bf5617c279db5a299e
Taykhoom Dalal <tdalal@g.ucla.edu>
2022-11-20 22:57:38 -0800

    import dars path

--- reactdjango/urls.py
+++ reactdjango/urls.py
@@ -17,11 +17,12 @@ from django.contrib import admin
 from django.urls import path, re_path
 from django.views.generic import TemplateView
 
-from sampleapp.views import getClasses_view
+from sampleapp.views import getClasses_view, importDars_view
 
 urlpatterns = [
   path('admin/', admin.site.urls),
   # path('api/', include('sampleapp.urls')),
   path('api/getClasses', getClasses_view),
+  path('api/importDars', importDars_view),
   re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
