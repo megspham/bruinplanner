52a9f03a4f91391bceaf1ac01685fd9b94ed68fe
iangalvez <iangalvez@g.ucla.edu>
2022-11-25 23:58:23 -0800

    changed butting formatting in calendar

--- src/components/SidebarButton.js
+++ src/components/SidebarButton.js
@@ -4,15 +4,16 @@
  */
 
 import React from "react"; 
+import './SidebarButton.css';
 /**
  * Create a CustomizedButton
  * @param {string} text What to write on the button
  * @param {function} onClick Defines what function to call when the button is clicked
  * @returns CustomizedButton HTML div object
  */
-export function SidebarButton({ width, height, text}) {
-    let style = {
-        width: '383.78px',
+export function SidebarButton({ width, height, text, kind}) {
+    let sidebar_style = {
+        width: '333.78px',
         height: '73.86px',
         background: '#FFFFFF',        
         fontFamily: 'Montserrat',
@@ -27,9 +28,25 @@ export function SidebarButton({ width, height, text}) {
         boxShadow: '0px 0px 11.0283px #8BB8E8',
         margin: '10px'
     }
+    let calendar_style = {
+        width: '200px',
+        height: '28px',
+        background: '#FFFFFF',        
+        fontFamily: 'Montserrat',
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: '20.7902px',
+        lineHeight: '28px',
+        border: 'none',
+        borderRadius: '3.68431px',
+        textTransform: 'uppercase',
+        color: '#005587',
+        boxShadow: '0px 0px 11.0283px #8BB8E8',
+        margin: '2px'
+    }
     return (
         // <div className="myButton">
-            <button type="button" style={style}>
+            <button type="button" style={kind === 'calendar' ? calendar_style : sidebar_style}>
                 {text}
             </button>
         // </div>
